ef0a02951c6238bf3b613008ed83c6b703d12d7e
R11T <romain.lecault@comalia.com>
2017-08-03 20:38:39 +0200

    Functional version \o/

--- src/checkbreak.go
+++ src/checkbreak.go
@@ -1,34 +1,21 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"path"
+	"regexp"
 	"strings"
 
+	"github.com/fatih/color"
+	"github.com/ferhatelmas/levenshtein"
 	"github.com/tbruyelle/qexec"
 )
 
 type checkBreak struct {
 	path   string
 	commit string
-}
-
-// BreakReport is a report to display
-type BreakReport struct {
-	files []FileReport
-}
-
-// FileReport is a filename and a slice of all potentials compatibility break
-type FileReport struct {
-	methods  []Method
-	filename string
-}
-
-// Method is a potential break on a public method
-type Method struct {
-	before   string
-	after    string
-	distance int
 }
 
 func (*checkBreak) init(path string, commit string) (*checkBreak, error) {
@@ -44,20 +31,52 @@ func (*checkBreak) init(path string, commit string) (*checkBreak, error) {
 	// not…
 }
 
+// BreakReport is a report to display
+type BreakReport struct {
+	files []FileReport
+}
+
+// FileReport is a pool of potentials compatibilities breaks
+type FileReport struct {
+	methods  []Method
+	filename string
+}
+
+func (fr *FileReport) report() {
+	fmt.Println(">", fr.filename)
+	for _, method := range fr.methods {
+		if "" == method.after {
+			fmt.Println("", color.RedString(method.before))
+		} else {
+			fmt.Println("", color.RedString(method.before), "->", color.GreenString(method.after))
+		}
+	}
+	fmt.Println("")
+}
+
 func (cb *checkBreak) report() (*BreakReport, error) {
-	gitFiles, err := qexec.Run("git", "diff", "--name-status", cb.commit) // branche de base
+	gitFiles, err := qexec.Run("git", "diff", "--name-status", cb.commit)
 	if nil != err {
 		return nil, err
 	}
 
-	files := filesToReport(gitFiles, *cb)
+	files := files(gitFiles, *cb)
 
 	filesReports := make([]FileReport, 0)
 	for _, file := range files {
-		fileReport := FileReport{
-			filename: file.name,
+		if !file.isUsefulFile() {
+			continue
 		}
-		filesReports = append(filesReports, fileReport)
+
+		methods, _ := file.breaks()
+
+		if 0 != len(*methods) {
+			fileReport := FileReport{
+				filename: file.name,
+				methods:  *methods,
+			}
+			filesReports = append(filesReports, fileReport)
+		}
 	}
 
 	return &BreakReport{
@@ -73,27 +92,91 @@ type File struct {
 	typeFile string
 }
 
-// Diff represents the diff of a file, segregated with deletion and adding
-type Diff struct {
-	deletions []string
-	addings   []string
+func (f *File) isUsefulFile() bool {
+	return "A" != f.status
 }
 
-func filesToReport(filenamesDiff string, cb checkBreak) []File {
+// Method is a potential break on a public method
+type Method struct {
+	before       string
+	after        string
+	commonFactor string
+	distance     int
+}
+
+func (f *File) breaks() (*[]Method, error) {
+	f.filterDiff()
+	pattern, err := patternByLanguage(f.typeFile)
+	if nil != err {
+		return nil, err
+	}
+
+	var methods []Method
+	var moveOnly bool
+	for _, deleted := range f.diff.deletions {
+		var closestAdding string
+		moveOnly = false
+		commonFactor := pattern.FindStringSubmatch(deleted)[0]
+		for _, added := range f.diff.addings {
+			if strings.HasPrefix(added, commonFactor) {
+				// It's only a move
+				if len(deleted) == len(added) {
+					moveOnly = true
+					break
+				} else {
+					closestAdding = added
+				}
+			}
+		}
+		if !moveOnly {
+			method := Method{
+				before:       deleted,
+				after:        closestAdding,
+				commonFactor: commonFactor,
+				distance:     levenshtein.Dist(deleted, closestAdding),
+			}
+			methods = append(methods, method)
+		}
+	}
+
+	return &methods, nil
+}
+
+func (f *File) filterDiff() error {
+	pattern, err := patternByLanguage(f.typeFile)
+	if nil != err {
+		return err
+	}
+	f.diff = Diff{
+		deletions: filteredByPattern(pattern, f.diff.deletions),
+		addings:   filteredByPattern(pattern, f.diff.addings),
+	}
+
+	return nil
+}
+
+func filteredByPattern(r *regexp.Regexp, data []string) []string {
+	filtered := make([]string, 0)
+	for _, element := range data {
+		if r.MatchString(element) {
+			filtered = append(filtered, element)
+		}
+	}
+
+	return filtered
+}
+
+func files(filenamesDiff string, cb checkBreak) []File {
 	files := make([]File, 0)
 
 	for _, fileLine := range strings.Split(strings.TrimSpace(filenamesDiff), "\n") {
-		if !isUsefulFile(fileLine) {
-			continue
-		}
-
 		file := &File{}
 		file.setStatus(fileLine)
 		file.setName(fileLine)
 		file.setType(fileLine)
 		file.setDiff(cb)
 
-		if isTypeDefined(*file) {
+		if file.hasAutorizedType() {
 			files = append(files, *file)
 		}
 	}
@@ -110,11 +193,17 @@ func (f *File) setName(fileLine string) {
 }
 
 func (f *File) setType(fileLine string) {
-	f.typeFile = path.Ext(fileLine)
+	f.typeFile = strings.TrimSpace(path.Ext(fileLine)[1:])
+}
+
+// Diff represents the diff of a file, segregated with deletion and adding
+type Diff struct {
+	deletions []string
+	addings   []string
 }
 
 func (f *File) setDiff(cb checkBreak) error {
-	diff, err := qexec.Run("git", "diff", cb.commit, f.name) // branche de base
+	diff, err := qexec.Run("git", "diff", cb.commit, f.name)
 	if nil != err {
 		return err
 	}
@@ -123,9 +212,9 @@ func (f *File) setDiff(cb checkBreak) error {
 	var diffAdded []string
 	for _, line := range strings.Split(diff, "\n") {
 		if strings.HasPrefix(line, "- ") {
-			diffDeleted = append(diffDeleted, line)
+			diffDeleted = append(diffDeleted, strings.TrimSpace(line[1:]))
 		} else if strings.HasPrefix(line, "+ ") {
-			diffAdded = append(diffAdded, line)
+			diffAdded = append(diffAdded, strings.TrimSpace(line[1:]))
 		}
 	}
 
@@ -136,19 +225,26 @@ func (f *File) setDiff(cb checkBreak) error {
 	return nil
 }
 
-func isUsefulFile(fileLine string) bool {
-	return !strings.HasPrefix(fileLine, "A")
-}
-
-func isTypeDefined(file File) bool {
-	types := []string{".go", ".php"}
-
-	for _, typeFile := range types {
-		if file.typeFile == typeFile {
-			return true
-		}
+func (f *File) hasAutorizedType() bool {
+	_, err := patternByLanguage(f.typeFile)
+	if nil != err {
+		return false
 	}
-	return false
+	return true
 }
 
-// construire une structure pour la récupération des BC en fonction des langages
+func patternByLanguage(extension string) (*regexp.Regexp, error) {
+	var pattern *regexp.Regexp
+	switch extension {
+	case "go":
+		pattern = regexp.MustCompile(`func [A-Za-z]*\(`)
+	case "php":
+		pattern = regexp.MustCompile(`^(\s)*public( static)? function [_A-Za-z]+\(`)
+	}
+
+	if nil == pattern {
+		return pattern, errors.New("Langage inconnu")
+	}
+
+	return pattern, nil
+}
--- src/main.go
+++ src/main.go
@@ -3,23 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
-
-	"github.com/ferhatelmas/levenshtein"
-)
-
-const (
-	// MaximumSimilarity represents how close two strings has to be to be considered a change on the same line
-	MaximumSimilarity = 20
 )
 
 var check *checkBreak
 
 func main() {
-	//args := os.Args[1:]
+	args := os.Args[1:]
 	// check error args
 
-	cb, errInit := check.init("/home/rlecault/Gesica", "topic/optimListFf...topic/optimListFf4Real")
+	cb, errInit := check.init(args[0], args[1])
 	if nil != errInit {
 		fmt.Println("Error :", errInit)
 		os.Exit(1)
@@ -34,105 +26,11 @@ func main() {
 		fmt.Println("Aucun cassage de compatibilité")
 		return
 	}
-	fmt.Println("/!\\ Cassage de compatibilité :")
+	separator := "#####################################"
+	fmt.Println(separator, "\nCassage de compatibilité potentiel")
+	fmt.Println("")
+
 	for _, fileReport := range breakReport.files {
-		fmt.Println("> ", fileReport.filename)
-		//fmt.Println(">> Suppression :")
-		for _, method := range fileReport.methods {
-			fmt.Println(method)
-		}
-		fmt.Println("")
+		fileReport.report()
 	}
-	return
-
-	//for _, fileLine := range files {
-
-	//breaks := getBreaks(fileLine, diff)
-	//report := FileReport{
-	//	filename: file.name,
-	//	methods:  getMethods(file),
-	//
-	//reports = append(reports, report)
-
-	//methods := file.methods(file.diff)
-
-	/*if 0 < len(diffDeleted) {
-		fmt.Println("\n> Cassage de compatibilité de", filename, ":")
-		if 0 == len(diffAdded) {
-			fmt.Println("|-> Suppression : ")
-			for _, suppression := range diffDeleted {
-				fmt.Println("|--> ", strings.TrimSpace(suppression[2:]))
-			}
-		} else {
-			fmt.Println("|-> Mutation : ")
-		}
-	}*/
-	//}
-	distance := levenshtein.Dist("bb", "acaib")
-	fmt.Println(distance)
-}
-
-/*
-func buildReport() BreakReport {
-	files := filesReport()
-
-	return BreakReport{
-		file:    file,
-		methods: getMethods(file),
-	}
-}
-*/
-
-func (f *File) filterDiff() {
-	pattern := getPattern(f.typeFile)
-	deletions := f.diff.deletions
-	for index, deletion := range deletions {
-		if pattern.MatchString(deletion) {
-			deletions = append(deletions[:index], deletions[index+1])
-		}
-	}
-	addings := f.diff.addings
-	for index, adding := range addings {
-		if pattern.MatchString(adding) {
-			addings = append(addings[:index], addings[index+1])
-		}
-	}
-}
-
-/*
-func getBreaks(pathFile string, diff string) *Method {
-	extension := path.Ext(pathFile)
-	pattern := getPattern(extension)
-
-	var results []string
-	if nil == pattern {
-		return nil
-	}
-	lines := strings.Split(diff, "\n")
-	for _, line := range lines {
-		if pattern.MatchString(line) {
-			results = append(results, line)
-
-		}
-	}
-	return Method{}
-
-}
-*/
-func getPattern(extension string) *regexp.Regexp {
-	var pattern *regexp.Regexp
-	switch extension {
-	case ".go":
-		pattern, _ = regexp.Compile(`func [A-Za-z]*\(`)
-	case ".php":
-		pattern, _ = regexp.Compile(`(\s)+public( static)? function [_A-Za-z]`)
-	}
-
-	return pattern
-}
-
-func getMethods(file File) []Method {
-	var methods []Method
-
-	return methods
 }
