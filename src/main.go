e64704ebcdfa11e5dc64b828ad275e5a1a635334
Prytoegrian <prytoegrian@protonmail.com>
2017-08-05 13:17:55 +0200

    Report ignored files

--- src/main.go
+++ src/main.go
@@ -19,22 +19,30 @@ func main() {
 	baseBranch := args[1]
 	workingBranch := args[2]
 	cb := check.init(path, baseBranch, workingBranch)
-	breakReport, errReport := cb.report()
-	if nil != errReport {
-		fmt.Println(errReport)
+	report, err := cb.report()
+	if nil != err {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 	title := "\nCheck-break report : " + cb.path + " " + cb.delta
 	fmt.Println(strings.Repeat("#", len(title)), strings.ToTitle(title))
 	fmt.Println()
-	if 0 == len(breakReport.files) {
+	if 0 == len(report.supported) {
 		fmt.Println("> Aucun cassage de compatibilité")
 		return
 	}
 	potential := "> Cassages de compatibilité potentiels"
 	fmt.Println(potential, "\n", strings.Repeat("-", len(potential)))
 
-	for _, fileReport := range breakReport.files {
-		fileReport.report()
+	for _, fileReport := range report.supported {
+		fmt.Println(fileReport.report())
+	}
+
+	if 0 != len(report.ignored) {
+		ignored := "> Fichiers ignorés"
+		fmt.Println(ignored, "\n", strings.Repeat("-", len(ignored)))
+		for _, fileIgnored := range report.ignored {
+			fmt.Println(fileIgnored.report())
+		}
 	}
 }
