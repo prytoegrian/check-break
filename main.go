90f1adbd8ff6fd98731c3b78ee9b1f060865d3fd
Prytoegrian <prytoegrian@protonmail.com>
2017-09-10 00:34:05 +0200

    Explode main

--- main.go
+++ main.go
@@ -21,14 +21,23 @@ func main() {
 	if errInit != nil {
 		log.Fatal("Init failed : ", errInit)
 	}
+	displayTitle(cb)
 	report, errReport := cb.report()
 	if errReport != nil {
 		log.Fatal("Error during report construction : ", errReport)
 	}
+	displayBreaks(report)
+	displayIgnored(report)
+}
+
+func displayTitle(cb *CheckBreak) {
 	title := "\nCheck-break report : " + cb.workingPath + " " + cb.startPoint + " " + cb.endPoint
 	fmt.Println(strings.Repeat("#", len(title)), title)
 	fmt.Println("For details, see https://github.com/Prytoegrian/check-break#what-is-a-compatibility-break-")
 	fmt.Println()
+}
+
+func displayBreaks(report *BreakReport) {
 	if 0 == len(report.supported) {
 		fmt.Println("> No compatibility break")
 	} else {
@@ -39,7 +48,9 @@ func main() {
 			fmt.Println(fileReport.report())
 		}
 	}
+}
 
+func displayIgnored(report *BreakReport) {
 	if 0 != len(report.ignored) {
 		ignored := "\n> Unsupported files"
 		fmt.Println(ignored, "\n", strings.Repeat("-", len(ignored)))
